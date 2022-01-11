4c778937204dde9d72f05574e3f6a34af6c968d1
Serg <pseesp@mail.ru>
2022-01-12 01:14:44 +0300

    min max file work

--- min_input.py
+++ min_input.py
@@ -2,9 +2,10 @@ from itertools import *
 
 nset = int(input())
 for i in range(nset):
-    # arr_list = []
+
     narr = int(input())
     *arr_list, = map(int, input().split())
+
     minval = None
     for i in product('+*', repeat=(narr - 1)):
         arr_list_cpy = arr_list.copy()
--- trop_min.py
+++ trop_min.py
@@ -18,7 +18,7 @@ def iter_signs(rep):
     return ls
 
 
-my_input_file = os.path.join(os.getcwd(), "trop_min1.txt")
+my_input_file = os.path.join(os.getcwd(), "trop_min.txt")
 if os.path.exists(my_input_file):
     # file exists
     logging.info("will take data from {}".format(my_input_file))
@@ -37,27 +37,40 @@ for i in range(nset):
     logging.info("narr = {} \narr_list ={}".format(narr, arr_list))
     minval = None
     for i in product('+*', repeat=(narr - 1)):
-        str4eval = ""
+        i = list(i)
         arr_list_cpy = arr_list.copy()
-        # logging.info("i = {} len(i) = {} i[0] = {}".format(i, len(i), i[0]))
+        logging.info("i = '{}'".format(i))
+        logging.info("arr_list = '{}'".format(arr_list))
         for j in range(len(i)):
             if i[j] == "*":
-                sumvalue = arr_list_cpy[j] + arr_list_cpy[j + 1]
-                arr_list_cpy[j] = sumvalue
-                arr_list_cpy[j + 1] = sumvalue
-            str4eval = str4eval + str(arr_list[j])
-            str4eval = str4eval + " "
-            str4eval = str4eval + i[j]
-            str4eval = str4eval + " "
-        str4eval = str4eval + str(arr_list[j + 1])
-        logging.info("str4eval = '{}'".format(str4eval))
+                summa = arr_list_cpy[j]
+                for k in range(j, len(i)):
+                    if i[k] == "*":
+                        summa += arr_list_cpy[k+1]
+                    else:
+                        break
+
+                for k in range(j, len(i)):
+                    if i[k] == "*":
+                        arr_list_cpy[k] = summa
+                        arr_list_cpy[k+1] = summa
+                    else:
+                        break
+
+                for k in range(j, len(i)):
+                    if i[k] == "*":
+                        i[k] = "+"
+                    else:
+                        break
+
         logging.info("arr_list_cpy = '{}'".format(arr_list_cpy))
         logging.info("min = '{}'".format(min(arr_list_cpy)))
+        minloc = min(arr_list_cpy)
         if minval is None:
-            minval = min(arr_list_cpy)
+            minval = minloc
         else:
-            if min(arr_list_cpy) < minval:
-                minval = min(arr_list_cpy)
+            if minloc < minval:
+                minval = minloc
     print(minval)
 
 if fromfile:
